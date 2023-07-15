177689ff0119a8fb2d7768f122e27c83a836e0ba
GTechSoftApp <88341190+GTechSoftApp@users.noreply.github.com>
2023-07-15 00:59:54 -0300

    alteracao sequelize

--- src/sql_connection/db_connection_server.ts
+++ src/sql_connection/db_connection_server.ts
@@ -1,23 +1,24 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize({
-  database: 'expo_favela',
-  dialect: 'mssql',
-  username: 'sa',
-  password: '##Heitor2021##',
-    dialectOptions: {
-    instanceName: 'SQLEXPRESS',
-    trustedConnection: true
+  database: "expo_favela",
+  dialect: "mssql",
+  username: "sa",
+  password: "##Heitor2021##",
+  dialectOptions: {
+    instanceName: "SQLEXPRESS",
+    trustedConnection: true,
   },
-    host: 'SERVER',
-port:49172
+  host: "SERVER",
+  port: 49172,
 });
 
 export async function iniciarConexao() {
   try {
     await sequelize.authenticate();
-    console.log('Conexão estabelecida com sucesso!');  
+    console.log("Conexão estabelecida com sucesso!");
+    console.log(await sequelize.query("select * from usuarios"));
   } catch (error) {
-    console.error('Erro ao conectar ao banco de dados:', error);
+    console.error("Erro ao conectar ao banco de dados:", error);
   }
 }
