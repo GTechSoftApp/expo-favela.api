8a163950211fa759841c5edb2b948bef445eac22
GTechSoftApp <88341190+GTechSoftApp@users.noreply.github.com>
2023-07-20 22:46:07 -0300

    api-expo-heroku-novo-npm

---
+++ src/server.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var cors = require("cors");
+var routes_1 = require("./routes");
+var funcao = require("firebase-functions");
+var connecttion_1 = require("./sql_connection/connecttion");
+var port = process.env.PORT || 3000;
+var app = express();
+app.use(express.json());
+app.use(routes_1["default"]);
+app.use(cors());
+(0, connecttion_1.initConnection)();
+app.listen(port, function () {
+    console.log("Servidor on!");
+});
+exports.api = funcao.https.onRequest(app);
