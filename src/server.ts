88d7803193a52eb33430ee0b33ff25dc02cf4f47
GTechSoftApp <88341190+GTechSoftApp@users.noreply.github.com>
2023-07-20 22:20:45 -0300

    api-expo-heroku

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import * as express from "express";
 import * as cors from "cors";
 import usersRoute from "./routes";
-
+import * as funcao from 'firebase-functions';
 import { initConnection } from "./sql_connection/connecttion";
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
@@ -13,3 +13,5 @@ initConnection();
 app.listen(port, () => {
   console.log("Servidor on!");
 });
+
+exports.api = funcao.https.onRequest(app);
