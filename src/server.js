fa93c5f94d888752289a7a2ebf4ed455b5eaeffc
GTechSoftApp <88341190+GTechSoftApp@users.noreply.github.com>
2023-07-21 21:51:59 -0300

    api-expo-update-serverjs

--- src/server.js
+++ src/server.js
@@ -3,25 +3,19 @@ exports.__esModule = true;
 var express = require("express");
 var cors = require("cors");
 var routes_1 = require("./routes");
-<<<<<<< HEAD
 var connecttion_1 = require("./sql_connection/connecttion");
 var port = process.env.PORT || 3333;
-=======
 var funcao = require("firebase-functions");
 var connecttion_1 = require("./sql_connection/connecttion");
->>>>>>> 24556952e0de65bfb813dcc9079454c11ddce843
 var app = express();
 app.use(express.json());
 app.use(routes_1["default"]);
 app.use(cors());
 (0, connecttion_1.initConnection)();
-<<<<<<< HEAD
 app.listen(port, function () {
     console.log("Servidor on!");
 });
-=======
 app.listen(3333, function () {
     console.log("Servidor on!");
 });
 exports.api = funcao.https.onRequest(app);
->>>>>>> 24556952e0de65bfb813dcc9079454c11ddce843
