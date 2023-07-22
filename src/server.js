c9da171be1608dac3a29b4e0d3965ea2c94050b8
GTechSoftApp <88341190+GTechSoftApp@users.noreply.github.com>
2023-07-21 21:57:03 -0300

    api-expo-removing-firebase-functions

--- src/server.js
+++ src/server.js
@@ -4,18 +4,11 @@ var express = require("express");
 var cors = require("cors");
 var routes_1 = require("./routes");
 var connecttion_1 = require("./sql_connection/connecttion");
-var port = process.env.PORT || 3333;
-var funcao = require("firebase-functions");
-var connecttion_1 = require("./sql_connection/connecttion");
 var app = express();
 app.use(express.json());
 app.use(routes_1["default"]);
 app.use(cors());
 (0, connecttion_1.initConnection)();
-app.listen(port, function () {
-    console.log("Servidor on!");
-});
 app.listen(3333, function () {
     console.log("Servidor on!");
 });
-exports.api = funcao.https.onRequest(app);
